25a8eaa964e333f56599c71510f586d1bb32f6ed
codewithmithun6 <codewithmithun6@gmail.com>
2025-03-03 20:42:07 +0530

    #03032025: joined two array List

---
+++ src/practice1/JoinArrayList.java
@@ -0,0 +1,33 @@
+package practice1;
+
+import java.util.ArrayList;
+
+public class JoinArrayList {
+    public static void main(String[] args) {
+
+        ArrayList<String> first_list = new ArrayList<>(){
+            {
+                add("Monday");
+                add("Tuesday");
+                add("Wednesday");
+                add("Thursday");
+
+            }
+        };
+
+        ArrayList<String> second_list = new ArrayList<>(){
+            {
+                add("Friday");
+                add("Saturday");
+                add("Sunday");
+
+            }
+        };
+
+        ArrayList<String> joined_list = new ArrayList<>();
+        joined_list.addAll(first_list);
+        joined_list.addAll(second_list);
+        System.out.println("Joined Array List: "+joined_list);
+
+    }
+}
