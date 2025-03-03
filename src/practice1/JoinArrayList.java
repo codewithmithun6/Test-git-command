7a24445b7dfb6512df8c113552e6e728b643a671
codewithmithun6 <codewithmithun6@gmail.com>
2025-03-03 22:37:20 +0530

    03032025: added student

--- src/practice1/JoinArrayList.java
+++ src/practice1/JoinArrayList.java
@@ -24,9 +24,19 @@ public class JoinArrayList {
             }
         };
 
+        ArrayList<String> student_list = new ArrayList<>(){
+            {
+                add("Hari");
+                add("Ramesh");
+                add("Sunny");
+
+            }
+        };
+
         ArrayList<String> joined_list = new ArrayList<>();
         joined_list.addAll(first_list);
         joined_list.addAll(second_list);
+        joined_list.addAll(student_list);
         System.out.println("Joined Array List: "+joined_list);
 
     }
